cfe6cfdaa316e0fd6e5ecacdbd0275f472df7996
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-06 19:06:25 -0600

    keep moving forward

--- script.js
+++ script.js
@@ -27,3 +27,16 @@ function next(){
     currentMonth =(currentMonth+1) %12;
     showCalendar(currentMonth,currentYear);
 }
+
+function previous(){
+    currentYear =(currentMonth === 0) ? currentYear-1: currentYear;
+    currentMonth=(currentMonth === 0) ? 11:currentMonth -1; 
+    showCalendar(currentMonth,currentYear);
+}
+
+function jump(){
+    currentYear = parseInt(selectYear.value);
+    currentMonth= parseInt(selectMonth.value);
+    showCalendar(currentMonth,currentYear);
+}
+
