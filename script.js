f159dcc5e15888f350423cd3750f6765c013a01f
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-06 18:46:34 -0600

    keep moving forward

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
+let today = new Date();
 
-let firstDay =(new Date(year,month)).getDay();
+//grabs the month
 
- function daysInMonth(imonth,iyear){
-     return 32- new Date(iyear,imonth,32).getDate();
- }
-
- let date =1;
- for(let i =0; i<6; i++){
-     // creates a table row
-    let row =document.createElement("tr");
-    // creating indiviual cells, fliling them up.
- for(let j= 0; j <7; j++){
-     if(i === 0 && j<firstDay){
-         cell = document.createElement("td");
-        CellText=document.createTextNode("");
-        cell.appendChild(celltext);
-        row.appendChild(cell);
-     }
-     else if(data > daysInMonth(month,year)){
-         break;
-     }
-
-     else{
-         cell =document.createElement("td");
-         cellText= document.createTextNode(date);
-         cell.appendChild(cellText);
-         row.appendChild(cell);
-         date++;
-     }
-
-   }
-   tbl.appendChild(row); // appending each row into calendar body.
- }
+let currentMonth =today.getMonth();
