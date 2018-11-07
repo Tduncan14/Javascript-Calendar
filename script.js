390024fcfc2a6b321bc944ebc460fd890a70d5a8
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-11-06 18:52:59 -0600

    keep moving forward

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let today = new Date();
 //grabs the month
 
 let currentMonth =today.getMonth();
+
+// grabs the current year
+
+let currentYear = today.getFullYear();
+
+let selectYear = document.getElementById("year");
+
+// grabs the element in the html
+
+let selectMonth = document.getElementById("month");
+
+// creating an array
+
+let months =["Jan","Feb","Mar","Apr","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
